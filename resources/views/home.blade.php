c569b1df08fa7143853fe3a8e83123f0d4ea7889
Finnedorn <daniele.giuntoli@outlook.com>
2023-12-19 15:57:49 +0100

    added basic scaffholding + comics db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,6 @@
 
 @section('content')
     <main>
-        <h1>
-            Home Page
-            <i class="fa fa-home"></i>
-        </h1>
+
     </main>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,98 @@
+@php
+   $dcComics = config("comics.dcComicsArr");
+   $dcShop = config("comics.shopArr");
+   $dcInfo = config("comics.dcArr");
+   $dcSites = config("comics.sitesArr");
+   $dcIcons = config("comics.iconsArr");
+@endphp
 <footer>
-    footer
+    <div class=" top-footer-wrapper position-relative  overflow-hidden">
+        <div class="container">
+            <div class="bg-logo-wrapper position-absolute ">
+                <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="dc-logo-bg">
+            </div>
+            <div class=" row-cols-6 d-flex py-5 ">
+                <!--col1-->
+                <div>
+                    <ul>
+                        <h5 class="font-my-light footer-title">
+                            DC COMICS
+                        </h5>
+                        @foreach ($dcComics as $item)
+                            <li>
+                                <a href="#" class="font-my-grey footer-links">
+                                    {{$item}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <ul>
+                        <h5 class="font-my-light footer-title">
+                            SHOP
+                        </h5>
+                        @foreach ($dcShop as $item)
+                            <li>
+                                <a href="#" class="font-my-grey footer-links">
+                                    {{$item}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!--col2-->
+                <div>
+                    <ul>
+                        <h5 class="font-my-light footer-title">
+                            DC
+                        </h5>
+                        @foreach ($dcInfo as $item)
+                            <li>
+                                <a href="#" class="font-my-grey footer-links">
+                                    {{$item}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!--col3-->
+                <div>
+                    <ul>
+                        <h5 class="font-my-light footer-title">
+                            SITES
+                        </h5>
+                        @foreach ($dcSites as $item)
+                            <li>
+                                <a href="#" class="font-my-grey footer-links">
+                                    {{$item}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-my-dark-grey py-4">
+        <div class="container d-flex  justify-content-between align-items-center">
+            <div class="ps-4">
+                <button type="button" class="btn btn-outline-primary font-my-light p-3">
+                    SIGN-UP NOW!
+                </button>
+            </div>
+            <div>
+                <ul class="d-flex flex-row flex-wrap justify-content-between align-items-center">
+                    <h5 class="footer-title font-my-light-blue pt-2">
+                        FOLLOW US
+                    </h5>
+                    @foreach ($dcIcons as $item)
+                        <li class=" ms-3 ">
+                            <a href="#" class="icon-wrapper">
+                                <img :src="{{$item['picture']}}" :alt="{{$item['info']}}">
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
